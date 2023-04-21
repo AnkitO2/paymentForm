92690508352d03fa856320a4e63ebcd23ba8b2d9
AnkitO2 <120965263+AnkitO2@users.noreply.github.com>
2023-04-21 23:20:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import MemberNav from'./MemberNav';
+// import MemberNavbyGrid from './MemberNavbyGrid';
+function App() {
+  return (
+    <div>
+   <MemberNav/>
+{/* <MemberNavbyGrid/> */}
+    </div>
+  )
+}
+
+export default App
